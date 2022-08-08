57778c83df3740e491d77bc9ce60c09147478bb4
Adam Wandoch <wandoch.adam@gmail.com>
2022-08-08 22:38:18 +0100

    tested Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import { StyledFooter } from './Footer.styled.jsx';
 
+const year = new Date().getFullYear();
+
 export const Footer = ({ colour }) => {
   return (
     <StyledFooter colour={colour}>
-      <p>
-        Adam Wandoch 2022 {'-> '}
+      <p data-testid='paragraph'>
+        Adam Wandoch {year} {'-> '}
         <a
           href='https://github.com/AdamWandoch/social-prototype'
           target='_blank'
---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import { Footer } from './Footer';
+
+const year = new Date().getFullYear();
+
+describe(Footer, () => {
+  it('can display the correct content', () => {
+    const { getByTestId } = render(<Footer />);
+    const paragraph = getByTestId('paragraph');
+    expect(paragraph.textContent).toEqual(`Adam Wandoch ${year} -> GitHub`);
+  });
+});
