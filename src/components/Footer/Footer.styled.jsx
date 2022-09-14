c74d2e57260cac014f857430ab29fcf77c51dde5
AdamWandoch <wandoch.adam@gmail.com>
2022-09-14 07:46:55 -0300

    fixed footer alignment

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const defaultColor = '#fff';
 
 export const StyledFooter = styled.footer`
-  text-align: center;
   font-size: small;
+  margin: auto;
   margin-top: 1rem;
   margin-bottom: 2rem;
   color: ${(props) => props.color || defaultColor};
