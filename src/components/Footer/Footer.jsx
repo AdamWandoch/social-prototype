2c997c3b7ca3d21eac572f6b9fd5a4e065e8c551
AdamWandoch <wandoch.adam@gmail.com>
2022-09-27 23:18:34 -0300

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,45 @@ const year = new Date().getFullYear();
 export const Footer = ({ color }) => {
   return (
     <StyledFooter color={color}>
-      <p data-testid='paragraph'>
-        Adam Wandoch {year} {'-> '}
-        <a
-          href='https://github.com/AdamWandoch/social-prototype'
-          target='_blank'
-          rel='noreferrer'
-        >
-          GitHub
-        </a>
-      </p>
+      <p data-testid='paragraph'>Adam Wandoch {year}</p>
+      <ul>
+        <li>
+          <a
+            href='https://www.linkedin.com/in/adam-wandoch/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://github.com/AdamWandoch/social-prototype'
+            target='_blank'
+            rel='noreferrer'
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.youtube.com/channel/UCVIH7IAFxmwSxYid9r_2R9w'
+            target='_blank'
+            rel='noreferrer'
+          >
+            YouTube
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.instagram.com/adamwandoch/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Instagram
+          </a>
+        </li>
+      </ul>
     </StyledFooter>
   );
 };
--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -4,8 +4,36 @@ const defaultColor = '#fff';
 
 export const StyledFooter = styled.footer`
   font-size: small;
+  text-align: center;
   margin: auto;
-  margin-top: 1rem;
-  margin-bottom: 2rem;
+  margin-top: 4rem;
   color: ${(props) => props.color || defaultColor};
+
+  p {
+    margin: 0;
+    text-align: center;
+  }
+
+  ul {
+    margin-bottom: 2rem;
+    list-style-type: none;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0;
+    margin-right: 1rem;
+  }
+
+  li:last-child {
+    margin-right: 0;
+  }
+
+  li::after {
+    /* content: ; */
+  }
+
+  li::before {
+    /* content: ' | '; */
+  }
 `;
